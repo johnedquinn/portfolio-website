3cd8793336743ce5756360da6ee5bf94057385f5
John Quinn <johnedquinn@gmail.com>
2019-12-28 06:00:56 -0500

    Initialize Courses Page

---
+++ src/components/pages/CoursesPage.js
@@ -0,0 +1,19 @@
+/// @file:      CoursesPage.js
+/// @author:    John Ed Quinn
+/// @desc:      Pieces together the Courses Page for the website by using smaller components
+
+/* IMPORTS */
+import React, { Fragment } from 'react';
+import Footer from '../layout/Footer';
+
+/* NAVBAR */
+const CoursesPage = () => {
+    return (
+        <Fragment>
+            <Footer />
+        </Fragment>
+    );
+}
+
+/* EXPORTS */
+export default CoursesPage;
