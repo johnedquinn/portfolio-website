78a6cc7de35340ce7ed6e2806486c3664c1fc1ac
John Quinn <johnedquinn@gmail.com>
2019-12-29 15:52:48 -0500

    Update icons and make sure link jumps work

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -23,22 +23,19 @@ const Footer = () => {
                 <div class="col">
                     <nav class="navbar navbar-expand-lg bg-transparent justify-content-center">
                         <ul class="navbar-nav">
-
-                            <li><a href="https://www.linkedin.com/in/johnedquinn/"><img className="footer-icon" src={LinkedIn}/></a></li>
-                            <li><a href="https://gitlab.com/johnedquinn"><img className="footer-icon" src={Github}/></a></li>
-                            <li><a href="https://gitlab.com/johnedquinn"><img className="footer-icon" src={Spotify}/></a></li>
-                            <li><a href="https://www.instagram.com/johnedquinn/"><img className="footer-icon" src={Instagram}/></a></li>
-                            <li><a href="https://www.snapchat.com/add/johnquinnn"><img className="footer-icon" src={Snapchat}/></a></li>
-                            <li><a href="https://twitter.com/johnedquinn"><img className="footer-icon" src={Twitter}/></a></li>
-                            <li class="nav-item px-2">
-                                <a class="nav-link text-light" rel="noopener noreferrer" target="_blank"
-                                    href="johnedquinn@gmail.com">Email</a>
-                            </li>
+                            <li><a href="https://www.linkedin.com/in/johnedquinn/" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="LinkedIn Icon" src={LinkedIn}/></a></li>
+                            <li><a href="https://gitlab.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Git Icon" src={Github}/></a></li>
+                            <li><a href="https://open.spotify.com/user/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Spotify Icon" src={Spotify}/></a></li>
+                            <li><a href="https://www.instagram.com/johnedquinn/" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Instagram Icon" src={Instagram}/></a></li>
+                            <li><a href="https://www.snapchat.com/add/johnquinnn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Snapchat Icon" src={Snapchat}/></a></li>
+                            <li><a href="https://twitter.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Twitter Icon" src={Twitter}/></a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
-            <div class="row py-5"></div>
+            <div class="row py-4"></div>
+                <p className="text-center"><small className="text-center text-white">&copy; 2019 John Ed Quinn</small></p>
+            <div class="row py-2"></div>
         </div>
     );
 }
