5b33ee03e4b8d2fa2bc5e89e388342aab0d466b0
John Quinn <johnedquinn@gmail.com>
2019-12-29 20:51:36 -0500

    Align content in center with mobile usage. Also remove beta statement.

--- src/components/layout/HomeHeader.js
+++ src/components/layout/HomeHeader.js
@@ -12,14 +12,12 @@ const HomeHeader = () => {
         <header className="header index-header">
             <div className="overlay"></div>
             <NavBar/>
-            <div class="container">
+            <div class="container mobile-center">
                 <div class="description">
                     <h1>JOHN</h1>
                     <h1>ED QUINN</h1>
                     <h3>Computer Science student at the University of Notre Dame.</h3>
                     <h5>Really just someone who is fascinated by what's possible.</h5>
-                    <br/>
-                    <p><small class="text-muted">This website is in beta. Please open on desktop - not mobile.</small></p>
                 </div>
             </div>
         </header>
