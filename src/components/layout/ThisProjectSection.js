275ae0a41f17aeb6fb244ded5555d3eded787b61
John Quinn <johnedquinn@gmail.com>
2019-12-29 21:18:42 -0500

    Move GitLab Project text to separate badge to keep consistency

--- src/components/layout/ThisProjectSection.js
+++ src/components/layout/ThisProjectSection.js
@@ -25,7 +25,7 @@ const ThisProjectSection = () => {
 							<div className="col col-7 mobile-center">
 								<h3 className="card-title text-dark"><a className="text-dark" rel="noopener noreferrer" target="_blank" href="https://gitlab.com/johnedquinn/portfolio-website">Portfolio Website</a> <img style={{maxHeight: '3rem', maxWidth: '1.5rem'}} src={GitLab_Favicon}
                                     alt="GitLab Project"></img></h3>
-                                <h6 className="card-text"><span className="badge badge-dark">GitLab Project | React | HTML | CSS | JS</span></h6>
+                                <h6 className="card-text"><span className="badge badge-dark">GitLab Project</span> <span className="badge badge-dark">React | HTML | CSS | JS</span></h6>
 								<p className="text-dark">This website uses NodeJS, React, HTML, CSS, JS, and Bootstrap to present to you the content that you are viewing at this very moment. The original website ran on a server (see DiscreetOctupus below), but was transferred to the current framework as part of a spur of curiosity mixed with freshly brewed coffee. Feel free to access the source code by clicking on the title above.</p>
 							</div>
 						</div>
