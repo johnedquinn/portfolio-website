a1eae1df5a6ebc4a4d7b822565bba173082b921e
John Quinn <johnedquinn@gmail.com>
2019-12-29 20:28:12 -0500

    Slight mods of references to class

--- src/components/layout/ThisProjectSection.js
+++ src/components/layout/ThisProjectSection.js
@@ -17,15 +17,15 @@ const ThisProjectSection = () => {
 					<div className="col">
 						<h1 className="text-center text-dark">This Project</h1>
 						<div className="row py-3"></div>
-						<div class="row py-3">
+						<div className="row py-3">
 							<div className="col col-4 text-center">
                                 <img className="project-image" src="https://assets.gitlab-static.net/uploads/-/system/project/avatar/15991469/BG.jpeg?width=40"
                                     alt="GitLab Project"></img>
 							</div>
 							<div className="col col-7">
-								<h3 class="card-title text-dark"><a class="text-dark" rel="noopener noreferrer" target="_blank" href="https://gitlab.com/johnedquinn/portfolio-website">Portfolio Website</a> <img style={{maxHeight: '3rem', maxWidth: '1.5rem'}} src={GitLab_Favicon}
+								<h3 className="card-title text-dark"><a className="text-dark" rel="noopener noreferrer" target="_blank" href="https://gitlab.com/johnedquinn/portfolio-website">Portfolio Website</a> <img style={{maxHeight: '3rem', maxWidth: '1.5rem'}} src={GitLab_Favicon}
                                     alt="GitLab Project"></img></h3>
-                                <h6 class="card-text"><span className="badge badge-dark">GitLab Project | React | HTML | CSS | JS</span></h6>
+                                <h6 className="card-text"><span className="badge badge-dark">GitLab Project | React | HTML | CSS | JS</span></h6>
 								<p className="text-dark">This website uses NodeJS, React, HTML, CSS, JS, and Bootstrap to present to you the content that you are viewing at this very moment. The original website ran on a server (see DiscreetOctupus below), but was transferred to the current framework as part of a spur of curiosity mixed with freshly brewed coffee. Feel free to access the source code by clicking on the title above.</p>
 							</div>
 						</div>
