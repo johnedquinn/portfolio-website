3d5662d4a04e23dc12f4745f2d3bd01a03e02b6d
John Quinn <johnedquinn@gmail.com>
2019-12-29 20:27:42 -0500

    Split all course badges up and change references of class to className

--- src/components/layout/MoreProjectsSection.js
+++ src/components/layout/MoreProjectsSection.js
@@ -12,7 +12,7 @@ import Projects from '../../assets/projects.json';
 /// @desc: returns a project card for the projects page
 const createProjectItem = (project) => {
 	// Check if should be shown
-	if (!project['valid']) return (<div></div>);
+	if (!project['valid']) return;
 
 	// Get Icon and Alternate Text
 	var icon; var alt_text;
@@ -24,17 +24,17 @@ const createProjectItem = (project) => {
 
 	// Return card
 	return (
-		<div class="row py-4">
+		<div className="row py-4">
 			<div className="col col-4 text-center">
 				<img className="project-image" src={project['imageURL']}
 					alt={alt_text}></img>
 			</div>
 			<div className="col col-8">
-				<h3 class="card-title text-dark"><a class="text-dark" rel="noopener noreferrer" target="_blank" href={project['link']}>{project['title']}</a>
+				<h3 className="card-title text-dark"><a className="text-dark" rel="noopener noreferrer" target="_blank" href={project['link']}>{project['title']}</a>
 					<img style={{ maxHeight: '3rem', maxWidth: '1.5rem', marginLeft: "10px" }} src={icon}
 						alt="GitLab Project"></img>
 				</h3>
-				<h6 class="card-text"><span className="badge badge-dark">{project['subtitle']}</span></h6>
+				<h6 className="card-text"><span className="badge badge-dark">{alt_text}</span> <span className="badge badge-dark">{project['subtitle']}</span></h6>
 				<p className="text-dark">{project['description']}</p>
 			</div>
 		</div>
@@ -59,7 +59,7 @@ const MoreProjectsSection = () => {
 				<div className="container">
 					<div className="row py-5"></div>
 					<h1 className="text-center text-dark">More Projects</h1>
-					<p class="text-dark text-center"><small class="text-muted">And <em>maybe</em> some playlists</small></p>
+					<p className="text-dark text-center"><small className="text-muted">And <em>maybe</em> some playlists</small></p>
 					<div className="row py-2"></div>
 					<ProjectsColumn/>
 					<div className="row py-5"></div>
