0458715cde686472f4a1eb33db4c994da74b0141
johnedquinn <johnedquinn@gmail.com>
2021-06-02 20:44:26 -0400

    Update to remove navbar

--- src/components/layout/HomeHeader.js
+++ src/components/layout/HomeHeader.js
@@ -11,13 +11,12 @@ const HomeHeader = () => {
     return (
         <header className="header index-header">
             <div className="overlay"></div>
-            <NavBar/>
             <div className="container mobile-center">
                 <div className="description">
                     <h1>JOHN</h1>
                     <h1>ED QUINN</h1>
                     <h3>Software Development Engineer at Amazon</h3>
-                    <h5>And just someone who is fascinated by what's possible</h5>
+                    <h5>Seattle, Washington, USA</h5>
                 </div>
             </div>
         </header>
