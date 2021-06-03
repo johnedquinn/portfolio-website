4d27ea56420bb2e9dab8082b163d8909401f2014
johnedquinn <johnedquinn@gmail.com>
2021-06-02 20:43:51 -0400

    Update Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,10 +6,10 @@
 import React from 'react';
 import LinkedIn from '../icons/LinkedIn.svg';
 import Github from '../icons/Github.svg';
-import Instagram from '../icons/Instagram.svg';
-import Twitter from '../icons/Twitter.svg';
 import Spotify from '../icons/Spotify.svg';
-import Snapchat from '../icons/Snapchat.svg';
+import Gitlab from '../icons/GitLab.png';
+import Resume from '../icons/Resume.png';
+import Email from '../icons/Email.png';
 
 /// @func: Footer
 /// @desc: Returns the footer for the website
@@ -24,18 +24,18 @@ const Footer = () => {
                 <div className="col">
                     <nav className="navbar navbar-expand bg-transparent justify-content-center">
                         <ul className="navbar-nav">
+                            <li><a href="mailto: johnedquinn@gmail.com" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Email Icon" src={Email}/></a></li>
                             <li><a href="https://www.linkedin.com/in/johnedquinn/" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="LinkedIn Icon" src={LinkedIn}/></a></li>
-                            <li><a href="https://gitlab.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Git Icon" src={Github}/></a></li>
+                            <li><a href="https://github.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Git Icon" src={Github}/></a></li>
+							<li><a href="https://gitlab.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="GitLab Icon" src={Gitlab}/></a></li>
+							<li><a href="https://drive.google.com/file/d/1rvOeW0j6wid5laUgBwpAPukAx5KJy5hN/view?usp=sharing" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Resume Icon" src={Resume}/></a></li>
                             <li><a href="https://open.spotify.com/user/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Spotify Icon" src={Spotify}/></a></li>
-                            <li><a href="https://www.instagram.com/johnedquinn/" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Instagram Icon" src={Instagram}/></a></li>
-                            <li><a href="https://www.snapchat.com/add/johnquinnn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Snapchat Icon" src={Snapchat}/></a></li>
-                            <li><a href="https://twitter.com/johnedquinn" rel="noopener noreferrer" target="_blank"><img className="footer-icon" alt="Twitter Icon" src={Twitter}/></a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
             <div className="row py-4"></div>
-                <p className="text-center"><small className="text-center text-white">&copy; 2019 John Ed Quinn</small></p>
+                <p className="text-center"><small className="text-center text-white">&copy; 2021 John Ed Quinn</small></p>
             <div className="row py-2"></div>
         </div>
     );
