2eb2d493f366cc063d9c2625e60ff1c0404628d6
John Ed Quinn <johqunn@amazon.com>
2022-08-11 23:19:10 -0700

    Updates

---
+++ precache-manifest.6f178c2b01f741d44f7f9009da2d3156.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c178d28fb5eaa5cfe7cbbeac11f6ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a26b1b6fb68a8c609a",
+    "url": "/static/css/main.f02975c9.chunk.css"
+  },
+  {
+    "revision": "41176aefa8e151be83c9",
+    "url": "/static/js/2.3e40a703.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3e40a703.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7a26b1b6fb68a8c609a",
+    "url": "/static/js/main.07d255fe.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "ba63e648d847935035aabf6f633a088e",
+    "url": "/static/media/Github.ba63e648.svg"
+  },
+  {
+    "revision": "4569f04dc315e5f7f84603b0e1f824fd",
+    "url": "/static/media/LinkedIn.4569f04d.svg"
+  },
+  {
+    "revision": "8ad120750e4e6bc49ef3e461b8a39a77",
+    "url": "/static/media/Spotify.8ad12075.svg"
+  },
+  {
+    "revision": "cb1ce59ecdaad6fa224f6092172cc717",
+    "url": "/static/media/paddle_board.cb1ce59e.jpg"
+  },
+  {
+    "revision": "dfcc537201b6e3881fc8331f8a1290c7",
+    "url": "/static/media/work.dfcc5372.jpeg"
+  }
+]);
